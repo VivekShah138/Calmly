5cc2a510c87c5e49c864f1d32f52fa4ca59d3836
vps5 <shahvivek138@gmail.com>
2025-07-28 00:57:49 +0100

    Sleep viewmodel added successfully

--- app/src/main/java/com/example/calmly/presentation/components/SleepScreen.kt
+++ app/src/main/java/com/example/calmly/presentation/components/SleepScreen.kt
@@ -1,38 +1,25 @@
 package com.example.calmly.presentation.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.calmly.navigation.BottomNavigationBar
-import com.example.calmly.presentation.viewmodel.MeditationViewModel
+import com.example.calmly.presentation.viewmodel.SleepViewModel
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun SleepScreen(
     navController: NavController,
-    viewModel: MeditationViewModel = koinViewModel()
+    viewModel: SleepViewModel = koinViewModel()
 ) {
-//    val currentPlayingSoundId by viewModel.currentPlayingSoundId.collectAsState()
-//
-//    LazyColumn {
-//        items(viewModel.sounds) { sound ->
-//            SoundCard(
-//                sound = sound,
-//                isPlaying = currentPlayingSoundId == sound.id,
-//                onPlayPauseClick = { viewModel.onPlayPauseClicked(sound) }
-//            )
-//        }
-//    }
+    val currentPlayingSoundId by viewModel.currentPlayingSoundId.collectAsState()
+
     Scaffold(
         bottomBar = {
             BottomNavigationBar(
@@ -40,16 +27,16 @@ fun SleepScreen(
             )
         }
     ) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
-        ){
-            Text(
-                text = "Sleep Screen"
-            )
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            items(viewModel.sounds) { sound ->
+                SoundCard(
+                    sound = sound,
+                    isPlaying = currentPlayingSoundId == sound.id,
+                    onPlayPauseClick = { viewModel.onPlayPauseClicked(sound) }
+                )
+            }
         }
     }
-
 }
